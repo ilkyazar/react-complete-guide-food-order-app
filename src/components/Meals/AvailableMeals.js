b680a6c0e0d2412153d005eae721d0029a9cdfa7
ilkyazar <ilkyaz.arabaci@gmail.com>
2022-06-12 18:37:53 +0300

    use card component in available meals

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,12 +1,16 @@
 import classes from './AvailableMeals.module.css';
 import { DUMMY_MEALS } from '../../lib/dummy-meals';
 
+import Card from '../UI/Card';
+
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
 
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
