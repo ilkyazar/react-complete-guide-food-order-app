5cb6117166bd06d5f318ae17b7ef3d8b5e82ea59
ilkyazar <ilkyaz.arabaci@gmail.com>
2022-06-12 20:59:54 +0300

    start adding cart reducer

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,6 +1,21 @@
+import { useReducer } from 'react';
 import CartContext from './cart-context';
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  return defaultCartState;
+};
+
 const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
   const addItemToCartHandler = (item) => {};
   const removeItemFromCartHandler = (id) => {};
 
