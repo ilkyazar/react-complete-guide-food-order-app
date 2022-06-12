956126302e59d7b1852f1e5a19b9cd0d9e061cf9
ilkyazar <ilkyaz.arabaci@gmail.com>
2022-06-12 18:44:28 +0300

    add meal item component

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -2,9 +2,17 @@ import classes from './AvailableMeals.module.css';
 import { DUMMY_MEALS } from '../../lib/dummy-meals';
 
 import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
 
   return (
     <section className={classes.meals}>
