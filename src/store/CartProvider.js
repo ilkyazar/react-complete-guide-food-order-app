90d616b48f1426ffb325e2b38f644e8053a0c529
ilkyazar <ilkyaz.arabaci@gmail.com>
2022-06-12 21:07:58 +0300

    dispatch cart actions, handle action type add

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -7,6 +7,13 @@ const defaultCartState = {
 };
 
 const cartReducer = (state, action) => {
+  if (action.type === 'ADD') {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+
+    return { items: updatedItems, totalAmount: updatedTotalAmount };
+  }
   return defaultCartState;
 };
 
@@ -16,12 +23,16 @@ const CartProvider = (props) => {
     defaultCartState
   );
 
-  const addItemToCartHandler = (item) => {};
-  const removeItemFromCartHandler = (id) => {};
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: 'ADD', item: item });
+  };
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: 'REMOVE', id: id });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
